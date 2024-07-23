8b3aaf53963ee295aa4fcdf597d89d6edf1282ab
John A. Marohn <jam99@cornell.edu>
2024-07-23 14:26:39 -0400

    Fix typo/bug in dissipationtheory/data2.py.

--- dissipationtheory/data2.py
+++ dissipationtheory/data2.py
@@ -5,7 +5,7 @@ from dissipationtheory.data import BLDSData
 from dissipationtheory.constants import ureg
 from dissipationtheory.dissipation import blds_perpendicular_jit
 from dissipationtheory.dissipation2 import theta1norm_jit
-from lmfit import Model, Parameters d
+from lmfit import Model, Parameters
 
 class BLDSData2(BLDSData):
 
